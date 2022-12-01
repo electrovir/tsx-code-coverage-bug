199b493ad3777478d96101940048310a6bfe1a65
electrovir <electrovir@users.noreply.github.com>
2022-12-01 14:56:57 +0000

    simplify configs further

--- configs/mocha.config.js
+++ configs/mocha.config.js
@@ -1,17 +1,8 @@
-const SpecReporterWithFileNames = require('mocha-spec-reporter-with-file-names');
-
 /** @type {import('mocha').MochaOptions} */
 const mochaConfig = {
-    checkLeaks: true,
-    color: true,
-    parallel: true,
-    slow: 1_500, // ms
-    fullTrace: true,
-    timeout: 60_000, // 1 minute in ms
-    reporter: SpecReporterWithFileNames.pathToThisReporter,
-    spec: [`src/**/!(*.type).test.ts?(x)`],
+  spec: [`src/**/*.test.ts`],
 
-    require: ['tsx'],
+  require: ['tsx'],
 };
 
 module.exports = mochaConfig;
--- configs/nyc.config.js
+++ configs/nyc.config.js
@@ -1,34 +1,16 @@
 const percent = 100;
 
 const nycConfig = {
-    extends: '@istanbuljs/nyc-config-typescript',
-    all: true,
-    branches: percent,
-    clean: true,
-    cache: false,
-    instrument: true,
-    // use the reporter (istanbul-smart-text-reporter) to fail instead of printing errors for
-    // every message
-    checkCoverage: false,
-    exclude: [
-        '**/*.test.ts',
-        '**/*.example.ts',
-        '**/*.type.test.ts',
-        '**/*.test-helper.ts',
-    ],
-    functions: percent,
-    include: ['src/**/*.ts'],
-    lines: percent,
-    // so we don't get error messages printed for every file
-    perFile: false,
-    reporter: [
-        'html',
-        'istanbul-smart-text-reporter',
-    ],
-    skipEmpty: true,
-    skipFull: true,
-    statements: percent,
-    tempDir: './node_modules/.nyc-output/',
+  extends: '@istanbuljs/nyc-config-typescript',
+
+  branches: percent,
+  functions: percent,
+  lines: percent,
+  statements: percent,
+
+  exclude: ['**/*.test.ts'],
+  include: ['src/**/*.ts'],
+  tempDir: './node_modules/.nyc-output/',
 };
 
 module.exports = nycConfig;
