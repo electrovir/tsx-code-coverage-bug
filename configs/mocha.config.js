a16ddffacc4c65259fe7e20650b5398a868d55af
electrovir <electrovir@users.noreply.github.com>
2022-12-01 14:43:39 +0000

    flatten configs

--- configs/mocha.config.js
+++ configs/mocha.config.js
@@ -1,8 +1,16 @@
-const baseOptions = require('virmator/base-configs/base-mocharc.js');
+const SpecReporterWithFileNames = require('mocha-spec-reporter-with-file-names');
 
 /** @type {import('mocha').MochaOptions} */
 const mochaConfig = {
-    ...baseOptions,
+    checkLeaks: true,
+    color: true,
+    parallel: true,
+    slow: 1_500, // ms
+    fullTrace: true,
+    timeout: 60_000, // 1 minute in ms
+    reporter: SpecReporterWithFileNames.pathToThisReporter,
+    spec: [`src/**/!(*.type).test.ts?(x)`],
+
     require: ['tsx'],
 };
 
--- configs/nyc.config.js
+++ configs/nyc.config.js
@@ -1,7 +1,34 @@
-const {getBaseConfigWithCoveragePercent} = require('virmator/base-configs/base-nyc.js');
+const percent = 100;
 
 const nycConfig = {
-    ...getBaseConfigWithCoveragePercent(100),
+    extends: '@istanbuljs/nyc-config-typescript',
+    all: true,
+    branches: percent,
+    clean: true,
+    cache: false,
+    instrument: true,
+    // use the reporter (istanbul-smart-text-reporter) to fail instead of printing errors for
+    // every message
+    checkCoverage: false,
+    exclude: [
+        '**/*.test.ts',
+        '**/*.example.ts',
+        '**/*.type.test.ts',
+        '**/*.test-helper.ts',
+    ],
+    functions: percent,
+    include: ['src/**/*.ts'],
+    lines: percent,
+    // so we don't get error messages printed for every file
+    perFile: false,
+    reporter: [
+        'html',
+        'istanbul-smart-text-reporter',
+    ],
+    skipEmpty: true,
+    skipFull: true,
+    statements: percent,
+    tempDir: './node_modules/.nyc-output/',
 };
 
 module.exports = nycConfig;
